1b0443f9ebb89bbda6ffa5d73d75c6ac734f3905
air_biker@abv.bg <air_biker@abv.bg@6b3ea05a-b322-11de-a210-8fd6ca7157a7>
2009-10-07 10:12:25 +0000

    Handles directories

--- decompile/JARdecompile.bat
+++ decompile/JARdecompile.bat
@@ -7,11 +7,23 @@ if "%EXT%"==".class" (
 	for %%D in (%1) do set IN=%%~D
 	goto l_class
 )
-%DIR%7za_zip.exe x -y -o"%OUT%" %1
+dir /ad "%1"
+echo %errorlevel%
+if errorlevel 1 (
+	%DIR%7za_zip.exe x -y -o"%OUT%" %1
+) else (
+	echo directory
+	set OUT=%1_decompiled
+	xcopy "%1" "%1_decompiled" /s /i
+)
+echo %OUT%
 for /D /R "%OUT%" %%F in (*) do (
 	%DIR%jad.exe -lnc -s .java -v -dead -o -d "%%F" "%%F\*.class" 
 	del "%%F\*.class" 
 )
+%DIR%jad.exe -lnc -s .java -v -dead -o -d "%OUT%" "%OUT%\*.class" 
+del "%OUT%\*.class" 
+	
 goto l_end
 
 :l_class
